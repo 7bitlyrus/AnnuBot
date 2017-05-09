d0d2bdc4c00297dff58fb5d39a30db8572e3d987
Ian Murray <theianm2002@ymail.com>
2017-05-09 10:38:56 -0400

    Remove unneeded config loads for guild join/leave events

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,6 @@
 var timehaze = require('timehaze');
 
 exports.func = (client, member) => {
-	serverconfig = client.loadGuildConfig(member.guild);
 	delta = timehaze.delta(member.user.createdAt, new Date());
 	client.logToGuild(member.guild, `:inbox_tray: ${client.formatUser(member.user)} joined the server (Account created ${delta.ago()})`);
 };
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,4 +1,3 @@
 exports.func = (client, member) => {
-	serverconfig = client.loadGuildConfig(member.guild);
 	client.logToGuild(member.guild, `:outbox_tray: ${client.formatUser(member.user)} left the server`);
 };
