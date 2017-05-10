de541badd4372c2554eba779e5b10cb51ab9e60f
Ian Murray <theianm2002@ymail.com>
2017-05-10 11:15:44 -0400

    Fix crash on message edit/delete because I'm stupid

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,7 +1,7 @@
 exports.func = (client, oldMessage, newMessage) => {
 	if(oldMessage.author.id == client.user.id) return;
 	svconf = client.loadGuildConfig(oldMessage.guild);
-	if(oldMessage.channel.id == svconf.instance.logs) return;
+	if(oldMessage.channel.id == svconf.logs) return;
 	if(oldMessage.content == newMessage.content) return; 
 	client.logToGuild(oldMessage.guild, `:pencil: ${client.formatUser(oldMessage.author)} message was edited in ${oldMessage.channel}:\n:b: ${oldMessage.cleanContent}\n:a: ${newMessage.cleanContent}`);
 };
