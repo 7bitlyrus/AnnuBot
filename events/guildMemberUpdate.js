5602752f2bc7a7e91fc2ed4471b6a1044ed5aadd
Ian Murray <theianm2002@ymail.com>
2017-08-07 19:33:02 -0400

    Log role changes

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -5,4 +5,23 @@ exports.func = (client, oldMember, newMember) => {
 
 		client.logToGuild(newMember.guild, `:name_badge: ${client.formatUser(newMember.user)} nickname was changed from ${oldFormatted} to ${newFormatted}`);
 	}
+	if(oldMember.roles != newMember.roles) {
+		oldArray = [];
+		newArray = [];
+
+		oldMember.roles.reduce((snowflake, role) => {
+			if(role.id == role.guild.id) return; // filters out @everyone
+			oldArray.push(role.name);
+		}, []);
+
+		newMember.roles.reduce((snowflake, role) => {
+			if(role.id == role.guild.id) return;
+			newArray.push(role.name);
+		}, []);
+
+		oldRoles = oldArray.join(', ') || "_(none)_";
+		newRoles = newArray.join(', ') || "_(none)_";
+
+		client.logToGuild(newMember.guild, `:lock_with_ink_pen: ${client.formatUser(newMember.user)} roles were changed:\n:b: ${oldRoles}\n:a: ${newRoles}`);
+	}
 };
