b0bb1af6b32df468a94d686e90a78d404d9f4da5
Ian Murray <theianm2002@ymail.com>
2017-08-09 21:33:47 -0400

    Rework log commands.

--- commands/getlog.js
+++ commands/getlog.js
@@ -1,21 +1,7 @@
 exports.func = function(client, msg, args) {
-	try {
-		if(!(msg.member.hasPermission("MANAGE_GUILD") || msg.author.id == client.config.instance.ownerid)) {
-			msg.reply(":x: Access denied.");
-			return;
-		}
-
-		serverconfig = client.loadGuildConfig(msg.guild);
-		if(serverconfig.logs) {
-			msg.reply(`The logging channel for this server is ${msg.guild.channels.get(serverconfig.logs)}`);
-		} else {
-			msg.reply(`This server has no logging channel.`);
-		}
-	} catch(e) {
-		console.warn(e);
-	}
+	msg.reply(`This command has been removed. Use \`${client.config.instance.prefix}log\`.`);
 };
 
-exports.description = "Returns the logging channel for the server. [Requires Manage Server permission]";
+exports.description = null;
 exports.allowedInDM = false;
-exports.displayHelp = true;
+exports.displayHelp = false;
--- commands/setlog.js
+++ commands/setlog.js
@@ -1,20 +1,7 @@
 exports.func = function(client, msg, args) {
-	try {
-		if(!(msg.member.hasPermission("MANAGE_GUILD") || msg.author.id == client.config.ownerid)) {
-			msg.reply(":x: Access denied.");
-			return;
-		}
-
-		serverconfig = client.loadGuildConfig(msg.guild);
-		serverconfig["logs"] = msg.channel.id;
-		client.writeGuildConfig(msg.guild, serverconfig);
-
-		msg.reply("This channel is now set as the logging channel for this server.")
-	} catch(e) {
-		console.warn(e);
-	}
+	msg.reply(`This command has been removed. Use \`${client.config.instance.prefix}log\`.`);
 };
 
-exports.description = "Sets the current channel as the logging channel for the server. [Requires Manage Server permission]";
+exports.description = null;
 exports.allowedInDM = false;
-exports.displayHelp = true;
+exports.displayHelp = false;
