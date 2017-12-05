20168fdd6c67ed13fc1e91565a2c2f8579180582
Ian Murray <theianm2002@ymail.com>
2017-12-04 20:41:32 -0500

    basic command parsing

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-const discord = require('discord.js');
-const config  = require('./config.json');
+const discord = require('discord.js')
+const config  = require('./config.json')
 
-const client = new discord.Client();
+const client = new discord.Client()
 
 client.on('ready', () => {
-
+	if(!config.prefix) config.prefix = `<@${client.user.id}> `
 });
 
 client.on('message', (msg) => {
-	console.log(msg)
+	if(msg.author.id == client.user.id) return
+	if(!msg.content.startsWith(config.prefix)) return
+
+	const args = msg.content.slice(config.prefix.length).split(" ");
+	const cmd = args.shift()
+
+	msg.reply(`<< DEBUG >>\nargs = ${args}\ncmd  = ${cmd}`, {code: "xl"})
 });
 
-client.on('debug', console.log);
-client.on('error', console.error);
-client.on('warn', console.warn);
-client.on('disconnect', console.warn);
+client.on('debug', console.log)
+client.on('error', console.error)
+client.on('warn', console.warn)
+client.on('disconnect', console.warn)
 
-client.login(config.token);
+client.login(config.token)
