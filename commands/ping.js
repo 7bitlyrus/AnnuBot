dcc58d1253d55847da4cd56af9bdc4d28f0826c5
Ian Murray <theianm2002@ymail.com>
2017-12-21 12:00:02 -0500

    misc style changes and future planning

--- commands/ping.js
+++ commands/ping.js
@@ -1,23 +1,23 @@
-const commandBase = require("../modules/commandBase");
+const commandBase = require('../modules/commandBase');
 const discord = require('discord.js')
 
 module.exports = class ping extends commandBase {
 	constructor() {
 		super()
-		this.aliases = ["pong"]
+		this.aliases = ['pong']
 	}
 
 	execute(msg) {
-		const embed = new discord.RichEmbed()
-		.setTitle("Pong!")
-		.addField("Heartbeat", `${Math.round(msg.client.ping)}ms`, true)
-		.addField("Heartbeat History", `${msg.client.pings.join("ms, ")}ms`, true)
-		.addField("Roundtrip", "\u200B", true)
+		let embed = new discord.RichEmbed()
+		.setTitle('Pong!')
+		.addField('Heartbeat', `${Math.round(msg.client.ping)}ms`, true)
+		.addField('Heartbeat History', `${msg.client.pings.join('ms, ')}ms`, true)
+		.addField('Roundtrip', '\u200B', true)
 
-		msg.channel.send("", embed).then(m => {
-			embed.fields[2] = {name: 'Roundtrip', value: `${m.createdTimestamp - msg.createdTimestamp}ms`, inline: true}
+		msg.channel.send('', embed).then(m => {
+			embed.fields[2] = {name: 'Roundtrip',value: `${m.createdTimestamp - msg.createdTimestamp}ms`, inline: true}
 
-			m.edit("", embed).catch(console.warn)
+			m.edit('', embed).catch(console.warn)
 		}).catch(console.warn)
 	}
 }
--- modules/database.js
+++ modules/database.js
@@ -1,5 +1,9 @@
 const Datastore = require('nedb')
+const util      = require('util');
 
-const db = new Datastore({ filename: 'db.db', autoload: true });
+const db = {}
+db.datastore = new Datastore({ filename: 'db.db', autoload: true });
+
+// const db.x = util.promisify(db.datastore.x);
 
 module.exports = db
