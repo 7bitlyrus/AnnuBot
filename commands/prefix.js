54301b569a48aca53b217c34e4bda860b14c8ea1
Ian Murray <theianm2002@ymail.com>
2017-12-21 12:19:39 -0500

    Add really basic logic for prefix command

--- commands/prefix.js
+++ commands/prefix.js
@@ -3,9 +3,16 @@ const commandBase = require('../modules/commandBase');
 module.exports = class prefix extends commandBase {
 	constructor() {
 		super()
-		this.disableDMs = true
+		this.disableDMs = "Prefixes are not used in direct messages."
 	}
-	execute(msg) {
-		msg.reply("bar")
+	execute(msg, args) {
+		msg.reply('wip command')
+		if(!args[0]) {
+			msg.reply('The prefix for this server is ...')
+		} else if(msg.member.hasPermission('MANAGE_GUILD')) {
+			msg.reply('Token set to ...')
+		} else {
+			msg.reply('You do not have permission to set the token for this server.')
+		}
 	}
 }
