3bd4991e7f8aad20ed2fd3863212f21a774d152f
Ian Murray <theianm2002@ymail.com>
2017-12-23 11:33:39 -0500

    add nedb-promise wrapper

--- modules/database.js
+++ modules/database.js
@@ -1,18 +1,16 @@
-const Datastore = require('nedb')
+const Datastore = require('nedb-promise')
 const util      = require('util');
 
 const db = new Datastore({ filename: 'db.db', autoload: true })
 
-db.ensureIDExists = (id) => new Promise((resolve, reject) => {
-	db.find({id: id}, (err, docs) => {
-		if(err) return reject(err)
-		if(docs.length) return resolve(docs[0])
+db.ensureIDExists = (id) => new Promise(async function(resolve, reject) {
+	let docs = await db.find({id: id})
+	if(docs.length) return resolve(docs[0])
 
-		db.insert({id: id}, (err, newDoc) => {
-			if(err) return reject(err)
-			if(newDoc) return resolve(newDoc)
-		})
-	})
+	let newDoc = db.insert({id: id})
+	if(newDoc) return resolve(newDoc)
+
+	reject()
 })
 
 module.exports = db
