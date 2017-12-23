9e5236764d63a26495e2f5ab17774898c4affa20
Ian Murray <theianm2002@ymail.com>
2017-12-23 11:01:15 -0500

    change to return a single object for db.ensureIDExists

--- modules/database.js
+++ modules/database.js
@@ -1,17 +1,16 @@
 const Datastore = require('nedb')
 const util      = require('util');
 
-// I tried to promisify database functions-- it really didn't like anything I tried. 
 const db = new Datastore({ filename: 'db.db', autoload: true })
 
 db.ensureIDExists = (id) => new Promise((resolve, reject) => {
 	db.find({id: id}, (err, docs) => {
 		if(err) return reject(err)
-		if(docs.length) return resolve(docs)
+		if(docs.length) return resolve(docs[0])
 
 		db.insert({id: id}, (err, newDoc) => {
 			if(err) return reject(err)
-			if(newDoc) return resolve([newDoc])
+			if(newDoc) return resolve(newDoc)
 		})
 	})
 })
