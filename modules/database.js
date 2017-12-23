598d8914260b39a0d5c11f8aa8fcc878f99bc61e
Ian Murray <theianm2002@ymail.com>
2017-12-23 10:15:21 -0500

    add db.ensureIDExists function

--- modules/database.js
+++ modules/database.js
@@ -1,9 +1,19 @@
 const Datastore = require('nedb')
 const util      = require('util');
 
-const db = {}
-db.datastore = new Datastore({ filename: 'db.db', autoload: true });
+// I tried to promisify database functions-- it really didn't like anything I tried. 
+const db = new Datastore({ filename: 'db.db', autoload: true })
 
-// const db.x = util.promisify(db.datastore.x);
+db.ensureIDExists = (id) => new Promise((resolve, reject) => {
+	db.find({id: id}, (err, docs) => {
+		if(err) return reject(err)
+		if(docs.length) return resolve(docs)
+
+		db.insert({id: id}, (err, newDoc) => {
+			if(err) return reject(err)
+			if(newDoc) return resolve([newDoc])
+		})
+	})
+})
 
 module.exports = db
