a7dbd0f16747ed7c53d6b645bf3ced3e7cc0dec4
Ian Murray <theianm2002@ymail.com>
2017-12-23 11:33:52 -0500

    allow updating prefix

--- commands/prefix.js
+++ commands/prefix.js
@@ -12,10 +12,20 @@ module.exports = class prefix extends commandBase {
 			let doc = await db.ensureIDExists(msg.guild.id)
 			msg.reply(doc.prefix ?
 				`This server's prefix is \`${doc.prefix}\`.` : 'This server does not have a prefix defined.')
+
 		} else if(msg.member.hasPermission('MANAGE_GUILD')) {
-			msg.reply('Not implemented.')
+
+			let doc = await db.ensureIDExists(msg.guild.id)
+
+			if(doc.prefix == args[0]) return msg.reply(`This server's prefix is already \`${doc.prefix}\`!`)
+
+			let numReplaced = await db.update({_id: doc._id}, {$set:{prefix: args[0]}})
+			let newDoc      = await db.ensureIDExists(msg.guild.id)
+
+			msg.reply(`This server's prefix is now \`${newDoc.prefix}\`.`)
+
 		} else {
-			msg.reply('You do not have permission to set the token for this server.')
+			msg.reply('You do not have permission to set the prefix for this server.')
 		}
 
 	}
